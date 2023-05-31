313f27bd5fe07d812c6c870d772aba43e3be3423
Arjun Biddanda <aabiddanda@gmail.com>
2023-05-31 15:22:08 -0400

    feat: sib inference of crossover now accounts for assay noise

--- analysis/crossovers/crossover.smk
+++ analysis/crossovers/crossover.smk
@@ -79,9 +79,7 @@ if Path("results/natera_inference/valid_trios.triplets.euploid.txt").is_file():
     with open("results/natera_inference/valid_trios.triplets.euploid.txt", "r") as fp:
         for i, line in enumerate(fp):
             [m, f, _] = line.rstrip().split()
-            total_data.append(
-                f"results/natera_inference/{m}+{f}.est_recomb.tsv"
-            )
+            total_data.append(f"results/natera_inference/{m}+{f}.est_recomb.tsv")
         total_data = np.unique(total_data).tolist()
 
 
@@ -151,23 +149,31 @@ rule filter_putative_euploid_triplets:
         "scripts/filter_euploid.py"
 
 
-def define_triplets(mother_id, father_id, trio_file='results/natera_inference/valid_trios.triplets.euploid.txt', base_path='/home/abiddan1/scratch16/natera_aneuploidy/analysis/aneuploidy/results/natera_inference'):
+def define_triplets(
+    mother_id,
+    father_id,
+    trio_file="results/natera_inference/valid_trios.triplets.euploid.txt",
+    base_path="/home/abiddan1/scratch16/natera_aneuploidy/analysis/aneuploidy/results/natera_inference",
+):
     trio_df = pd.read_csv(trio_file, sep="\t")
     filt_df = trio_df[(trio_df.mother == mother_id) & (trio_df.father == father_id)]
     res = []
     for c in filt_df.child.values:
-        res.append(f'{base_path}/{mother_id}+{father_id}/{c}.bafs.pkl.gz')
+        res.append(f"{base_path}/{mother_id}+{father_id}/{c}.bafs.pkl.gz")
     return res
+
 
 rule estimate_recombination_euploid_trio:
     """Estimate crossover events in euploid trio datasets."""
     input:
         euploid_triplets="results/natera_inference/valid_trios.triplets.euploid.txt",
-        baf_pkl = lambda wildcards: define_triplets(mother_id = wildcards.mother, father_id=wildcards.father)
+        baf_pkl=lambda wildcards: define_triplets(
+            mother_id=wildcards.mother, father_id=wildcards.father
+        ),
     output:
-        est_recomb="results/natera_inference/{mother}+{father}.est_recomb.tsv"
+        est_recomb="results/natera_inference/{mother}+{father}.est_recomb.tsv",
     params:
-        chroms=chroms
+        chroms=chroms,
     resources:
         time="1:00:00",
         mem_mb="5G",
--- analysis/crossovers/scripts/sibling_hmm.py
+++ analysis/crossovers/scripts/sibling_hmm.py
@@ -1,59 +1,71 @@
 import gzip as gz
 import pickle
 import sys
-import numpy as np
 from pathlib import Path
-from tqdm import tqdm
+
+import numpy as np
 from karyohmm import QuadHMM
+from tqdm import tqdm
+
 
 def load_baf_data(baf_pkls):
     """Load in the multiple BAF datasets."""
     family_data = {}
     for fp in baf_pkls:
-        embryo_name = Path(fp).stem.split('.')[0]
-        with gz.open(fp, 'rb') as f:
+        embryo_name = Path(fp).stem.split(".")[0]
+        with gz.open(fp, "rb") as f:
             data = pickle.load(f)
             family_data[embryo_name] = data
     return family_data
 
-def prepare_paired_data(embryo_id1='10013440016_R06C01', embryo_id2='10013440016_R04C01', embryo_id3='10013440016_R05C01', chrom='chr21', data_dict=None):
+
+def prepare_paired_data(
+    embryo_id1="10013440016_R06C01",
+    embryo_id2="10013440016_R04C01",
+    embryo_id3="10013440016_R05C01",
+    chrom="chr21",
+    data_dict=None,
+):
     """Create the filtered dataset for evaluating crossovers using the QuadHMM results."""
     data_embryo1 = data_dict[embryo_id1][chrom]
     data_embryo2 = data_dict[embryo_id2][chrom]
     data_embryo3 = data_dict[embryo_id3][chrom]
-    if (data_embryo1['pos'].size != data_embryo2['pos'].size) or ((data_embryo2['pos'].size != data_embryo3['pos'].size)):
-        pos1 = data_embryo1['pos']
-        pos2 = data_embryo2['pos']
-        pos3 = data_embryo3['pos']
+    if (data_embryo1["pos"].size != data_embryo2["pos"].size) or (
+        (data_embryo2["pos"].size != data_embryo3["pos"].size)
+    ):
+        pos1 = data_embryo1["pos"]
+        pos2 = data_embryo2["pos"]
+        pos3 = data_embryo3["pos"]
         idx2 = np.isin(pos2, pos1) & np.isin(pos2, pos3)
         idx1 = np.isin(pos1, pos2) & np.isin(pos1, pos3)
         idx3 = np.isin(pos3, pos1) & np.isin(pos3, pos2)
-        baf1 = data_embryo1['baf_embryo'][idx1]
-        baf2 = data_embryo2['baf_embryo'][idx2]
-        baf3 = data_embryo3['baf_embryo'][idx3]
-        mat_haps = data_embryo1['mat_haps'][:,idx1]
-        pat_haps = data_embryo1['pat_haps'][:,idx1]
+        baf1 = data_embryo1["baf_embryo"][idx1]
+        baf2 = data_embryo2["baf_embryo"][idx2]
+        baf3 = data_embryo3["baf_embryo"][idx3]
+        mat_haps = data_embryo1["mat_haps"][:, idx1]
+        pat_haps = data_embryo1["pat_haps"][:, idx1]
         assert baf1.size == baf2.size
         assert baf2.size == baf3.size
         # Return the maternal haplotypes, paternal haplotypes, baf
         return mat_haps, pat_haps, baf1, baf2, baf3, pos1[idx1]
     else:
-        pos = data_embryo1['pos']
-        baf1 = data_embryo1['baf_embryo']
-        baf2 = data_embryo2['baf_embryo']
-        baf3 = data_embryo3['baf_embryo']
-        mat_haps = data_embryo1['mat_haps']
-        pat_haps = data_embryo1['pat_haps']
+        pos = data_embryo1["pos"]
+        baf1 = data_embryo1["baf_embryo"]
+        baf2 = data_embryo2["baf_embryo"]
+        baf3 = data_embryo3["baf_embryo"]
+        mat_haps = data_embryo1["mat_haps"]
+        pat_haps = data_embryo1["pat_haps"]
         # Return the maternal haplotypes, paternal haplotypes, baf
         assert baf1.size == baf2.size
         assert baf2.size == baf3.size
         return mat_haps, pat_haps, baf1, baf2, baf3, pos
 
+
 def find_nearest_het(idx, pos, haps):
     """Find the nearest heterozygotes to the estimated crossover position."""
     assert idx >= 0 and idx <= haps.shape[1]
     assert pos.size == haps.shape[1]
-    geno = haps[0,:] + haps[1,:]
+    geno = haps[0, :] + haps[1, :]
     het_idx = np.where(geno == 1)[0]
     if idx < np.min(het_idx):
         left_boundary = np.nan
@@ -65,10 +77,11 @@ def find_nearest_het(idx, pos, haps):
         right_boundary = pos[het_idx[het_idx >= idx][0]]
     return left_boundary, pos[idx], right_boundary
 
+
 if __name__ == "__main__":
     # Read in the input data and params ...
     hmm = QuadHMM()
-    family_data = load_baf_data(snakemake.input['baf_pkl'])
+    family_data = load_baf_data(snakemake.input["baf_pkl"])
     names = [k for k in family_data.keys()]
     nsibs = len(names)
     lines = []
@@ -76,19 +89,53 @@ if __name__ == "__main__":
         for i in range(nsibs):
             j = (i + 1) % nsibs
             j2 = (i + 2) % nsibs
-            mat_haps, pat_haps, baf0, baf1, baf2, pos = prepare_paired_data(embryo_id1=names[i], embryo_id2=names[j], embryo_id3=names[j2], chrom=c, data_dict=family_data)
-            path_01, _, _,_ = hmm.viterbi_algorithm(bafs=[baf0, baf1], mat_haps=mat_haps, pat_haps=pat_haps, r=1e-18)
+            mat_haps, pat_haps, baf0, baf1, baf2, pos = prepare_paired_data(
+                embryo_id1=names[i],
+                embryo_id2=names[j],
+                embryo_id3=names[j2],
+                chrom=c,
+                data_dict=family_data,
+            )
+            pi0_01, sigma_01 = hmm.est_sigma_pi0(
+                bafs=[baf0, baf1], mat_haps=mat_haps, pat_haps=pat_haps, r=1e-18
+            )
+            path_01, _, _, _ = hmm.viterbi_algorithm(
+                bafs=[baf0, baf1],
+                mat_haps=mat_haps,
+                pat_haps=pat_haps,
+                pi0=pi0_01,
+                std_dev=sigma_01,
+                r=1e-18,
+            )
             refined_path_01 = hmm.restrict_path(path_01)
-            path_02, _, _,_ = hmm.viterbi_algorithm(bafs=[baf0, baf2], mat_haps=mat_haps, pat_haps=pat_haps, r=1e-18)
+            pi0_02, sigma_02 = hmm.est_sigma_pi0(
+                bafs=[baf0, baf2], mat_haps=mat_haps, pat_haps=pat_haps, r=1e-18
+            )
+            path_02, _, _, _ = hmm.viterbi_algorithm(
+                bafs=[baf0, baf2],
+                mat_haps=mat_haps,
+                pat_haps=pat_haps,
+                pi0=pi0_02,
+                std_dev=sigma_02,
+                r=1e-18,
+            )
             refined_path_02 = hmm.restrict_path(path_02)
-            mat_rec, pat_rec = hmm.isolate_recomb(refined_path_01, refined_path_02)
+            mat_rec, pat_rec = hmm.isolate_recomb(
+                refined_path_01, refined_path_02, window=20
+            )
             for m in mat_rec:
-                left, rec_pos, right = find_nearest_het(m[0], pos, mat_haps)                
-                lines.append(f'{snakemake.wildcards["mother"]}\t{snakemake.wildcards["father"]}\t{names[i]}\t{c}\tmaternal\t{left}\t{rec_pos}\t{right}\n')
+                left, rec_pos, right = find_nearest_het(m[0], pos, mat_haps)
+                lines.append(
+                    f'{snakemake.wildcards["mother"]}\t{snakemake.wildcards["father"]}\t{names[i]}\t{c}\tmaternal\t{left}\t{rec_pos}\t{right}\t{np.mean([pi0_01, pi0_02])}\t{np.mean([sigma_01, sigma_02])}\n'
+                )
             for p in pat_rec:
-                left, rec_pos, right = find_nearest_het(p[0], pos, pat_haps)                
-                lines.append(f'{snakemake.wildcards["mother"]}\t{snakemake.wildcards["father"]}\t{names[i]}\t{c}\tpaternal\t{left}\t{rec_pos}\t{right}\n')
-    with open(snakemake.output['est_recomb'], 'w') as out:
-        out.write('mother\tfather\tchild\tchrom\tcrossover_sex\tmin_pos\tavg_pos\tmax_pos\n')
+                left, rec_pos, right = find_nearest_het(p[0], pos, pat_haps)
+                lines.append(
+                    f'{snakemake.wildcards["mother"]}\t{snakemake.wildcards["father"]}\t{names[i]}\t{c}\tpaternal\t{left}\t{rec_pos}\t{right}\t{np.mean([pi0_01, pi0_02])}\t{np.mean([sigma_01, sigma_02])}\n'
+                )
+    with open(snakemake.output["est_recomb"], "w") as out:
+        out.write(
+            "mother\tfather\tchild\tchrom\tcrossover_sex\tmin_pos\tavg_pos\tmax_pos\tavg_pi0\tavg_sigma\n"
+        )
         for line in lines:
             out.write(line)
