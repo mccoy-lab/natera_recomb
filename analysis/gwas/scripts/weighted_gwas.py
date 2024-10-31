0e5e461b27c2b669e12629def9aba1b0fba5eace
Arjun Biddanda <aabiddanda@gmail.com>
2024-10-30 20:41:08 -0400

    draft: small script to run weighted OLS for GWAS

---
+++ analysis/gwas/scripts/weighted_gwas.py
@@ -0,0 +1,16 @@
+import tqdm as tqdm
+import pgenlib
+
+
+def perform_regression(ys, xs, cs):
+    """Perform weighted linear regression."""
+    pass
+
+
+def extract_statistics(xs):
+    """Extract some variant-level statistics."""
+    pass
+
+
+if __name__ == "__main__":
+    pass
